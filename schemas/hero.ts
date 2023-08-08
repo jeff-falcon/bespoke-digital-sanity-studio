99de5bb67dce9a99c5aa3715b28dd542b42f326d
Jeff Falcon <jeff@igloo.net>
2023-08-08 14:26:13 -0400

    add video to hero

--- schemas/hero.ts
+++ schemas/hero.ts
@@ -1,7 +1,13 @@
+import { defineField, defineType } from 'sanity'
 import { makeCloudinaryThumb } from "../lib/util"
 
+interface FieldParams {
+  parent: any
+  value: any
+}
+
 // schemas/project.ts
-export default {
+export default defineType({
   name: 'hero',
   type: 'document',
   title: 'Hero',
@@ -20,36 +26,54 @@ export default {
     },
   },
   fields: [
-    {
+    defineField({
       name: 'name',
       type: 'string',
-      title: 'Name',
-    },
-    {
-      name: 'client',
+      title: 'Title',
+    }),
+    defineField({
+      name: 'subtitle',
       type: 'string',
-      title: 'Client',
-    },
-    {
-      name: 'category',
-      type: 'string',
-      title: 'Category',
-    },
-    {
+      title: 'Subtitle',
+    }),
+    defineField({
       name: 'image_desktop',
       type: 'cloudinary.asset',
       title: 'Desktop Image',
-    },
-    {
+    }),
+    defineField({
       name: 'image_mobile',
       type: 'cloudinary.asset',
       title: 'Mobile Image',
-    },
-    {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-    },
+    }),
+    defineField({
+      name: 'kind',
+      type: 'string',
+      title: 'Thumbnail type',
+      initialValue: 'image',
+      options: {
+        list: [
+          { title: 'Image', value: 'image' },
+          { title: 'Video BG', value: 'video-bg' },
+        ],
+        layout: 'radio',
+        direction: 'horizontal',
+      },
+    }),
+    defineField({
+      name: 'thumb_vimeo_src',
+      type: 'string',
+      title: 'Thumbnail video: 720p Vimeo MP4 URL',
+      description: 'Use a 720p MP4 file from Vimeo’s "Video file links"',
+      hidden: ({ parent, value }: FieldParams) => parent.kind !== 'video-bg',
+    }),
+    defineField({
+      name: 'thumb_vimeo_src_hd',
+      type: 'string',
+      title: 'Thumbnail video: 1080p Vimeo MP4 URL',
+      description: 'Use a 1080p MP4 file from Vimeo’s "Video file links"',
+      hidden: ({ parent, value }: FieldParams) => parent.kind !== 'video-bg',
+    }),
     {
       name: 'project',
       title: 'Project',
@@ -57,4 +81,4 @@ export default {
       to: [{ type: 'project' }],
     },
   ],
-}
+})
