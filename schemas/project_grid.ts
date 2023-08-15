e4efc76ecbb01f0eecb29d1bfa41780ef1260a96
Jeff Falcon <jeff@igloo.net>
2023-08-15 02:07:20 -0400

    add options

--- schemas/project_grid.ts
+++ schemas/project_grid.ts
@@ -1,15 +1,39 @@
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { defineArrayMember, defineField, defineType } from 'sanity'
 
 // schemas/project.ts
 export default defineType({
   name: 'project_grid',
   type: 'document',
   title: 'Project Grid',
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'title',
+    },
+    prepare(value, viewOptions) {
+      const { title, subtitle } = value as { title: string, subtitle: string }
+      return {
+        title,
+        subtitle: subtitle ? `Section title: ${subtitle}` : '',
+      }
+    },
+  },
   fields: [
     defineField({
       name: 'name',
       type: 'string',
       title: 'Name',
+    }),
+    defineField({
+      name: 'title',
+      type: 'string',
+      title: 'Section Title',
+    }),
+    defineField({
+      name: 'feature_first',
+      type: 'boolean',
+      title: 'Feature first project',
+      description: 'If checked, the first project will be featured larger. You should have an odd number of projects in the grid for this to work well.',
     }),
     defineField({
       name: 'projects',
@@ -20,7 +44,7 @@ export default defineType({
           name: 'project',
           title: 'Project',
           type: 'reference',
-          to: [{type: 'project'}],
+          to: [{ type: 'project' }],
         }),
       ],
     }),
